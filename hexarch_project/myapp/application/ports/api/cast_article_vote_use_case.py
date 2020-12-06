14620519c6b3cc7d91e53b184b21e1feb6a09f1f
Zaur Nasibov <zaur@znasibov.info>
2020-12-06 20:32:38 +0200

    Start hexagonal architecture helper project for the blog post

---
+++ hexarch_project/myapp/application/ports/api/cast_article_vote_use_case.py
@@ -0,0 +1,14 @@
+from typing import Protocol
+from uuid import UUID
+
+from myapp.application.domain.model.vote import Vote
+from myapp.application.domain.model.cast_article_vote_result import (
+    CastArticleVoteResult,
+)
+
+
+class CastArticleVoteUseCase(Protocol):
+    def cast_article_vote(
+        self, user_id: UUID, post_id: UUID, vote: Vote
+    ) -> CastArticleVoteResult:
+        pass
---
+++ hexarch_project/myapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HexarchAppConfig(AppConfig):
+    name = 'hexarch_app'
