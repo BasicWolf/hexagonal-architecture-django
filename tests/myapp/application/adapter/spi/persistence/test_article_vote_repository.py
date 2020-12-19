8f468fb945ce3821ed8b515f9ffb00bad03026e7
Zaur Nasibov <zaur@znasibov.info>
2020-12-19 21:07:16 +0200

    Clean test_article_vote_repository.py up and add proper saving article vote test

--- tests/myapp/application/adapter/spi/persistence/test_article_vote_repository.py
+++ tests/myapp/application/adapter/spi/persistence/test_article_vote_repository.py
@@ -1,50 +1,45 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 import pytest
-from django.db import transaction, IntegrityError
+from django.db import connection
 
 from myapp.application.adapter.spi.persistence.article_vote_repository import \
     ArticleVoteRepository
+from myapp.application.adapter.spi.persistence.entity.article_vote import \
+    ArticleVoteEntity
 from myapp.application.domain.model.article_vote import ArticleVote
 from myapp.application.domain.model.vote import Vote
+from tests.myapp.application.adapter.spi.persistence.namedtuple_fetchall import \
+    namedtuple_fetchall
 
 
-@transaction.atomic
-def hoho():
-    article_vote_repository = ArticleVoteRepository()
+@pytest.fixture
+def article_id() -> UUID:
+    return uuid4()
 
-    print('11111111111111111111111111')
-    article_vote_repository.save_article_vote(
-        ArticleVote(
-            user_id=UUID('76bc279b-883f-4809-a030-e069d3517b5c'),
-            post_id=UUID('e55e700e-9fae-4954-872a-675757112fdc'),
-            vote=Vote.UP
-        )
-    )
-    print('22222222222222222222222222222')
-    try:
-        article_vote_repository.save_article_vote(
-            ArticleVote(
-                user_id=UUID('76bc279b-883f-4809-a030-e069d3517b5c'),
-                post_id=UUID('e55e700e-9fae-4954-872a-675757112fdc'),
-                vote=Vote.UP
-            )
-        )
-    except IntegrityError:
-        return 10
 
-    print('3333333333333333333333333333333')
-    article_vote_repository.save_article_vote(
-        ArticleVote(
-            user_id=UUID('76bc279b-883f-4809-a030-e069d3517b5c'),
-            post_id=UUID('e55e700e-9fae-4954-872a-675757112fdc'),
-            vote=Vote.UP
-        )
-    )
-    print('44444444444444444444444444444444')
+@pytest.fixture
+def user_id() -> UUID:
+    return uuid4()
+
 
 @pytest.mark.django_db
-def test_save_article_vote():
-    assert hoho() == 10
+def test_save_article_vote(user_id: UUID, article_id: UUID):
+    article_vote_repository = ArticleVoteRepository()
 
+    article_vote_repository.save_article_vote(
+        ArticleVote(
+            user_id=user_id,
+            article_id=article_id,
+            vote=Vote.UP
+        )
+    )
 
+    with connection.cursor() as cursor:
+        cursor.execute(f'SELECT user_id, article_id, vote from '
+                       f'{ArticleVoteEntity._meta.db_table}')
+        aritcle_vote = namedtuple_fetchall(cursor)[0]
+
+        assert user_id == UUID(aritcle_vote.user_id)
+        assert article_id == UUID(aritcle_vote.article_id)
+        assert 1 == aritcle_vote.vote
