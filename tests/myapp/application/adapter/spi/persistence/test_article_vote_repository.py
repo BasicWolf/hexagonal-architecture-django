b3eeb219db7b4ddc867901b3d128896914957e3f
Zaur Nasibov <zaur@znasibov.info>
2020-12-19 21:49:04 +0200

    Test that integrity error is raised when two identical article votes are saved

--- tests/myapp/application/adapter/spi/persistence/test_article_vote_repository.py
+++ tests/myapp/application/adapter/spi/persistence/test_article_vote_repository.py
@@ -1,7 +1,7 @@
 from uuid import UUID, uuid4
 
 import pytest
-from django.db import connection
+from django.db import connection, IntegrityError
 
 from myapp.application.adapter.spi.persistence.article_vote_repository import \
     ArticleVoteRepository
@@ -38,8 +38,28 @@ def test_save_article_vote(user_id: UUID, article_id: UUID):
     with connection.cursor() as cursor:
         cursor.execute(f'SELECT user_id, article_id, vote from '
                        f'{ArticleVoteEntity._meta.db_table}')
-        aritcle_vote = namedtuple_fetchall(cursor)[0]
+        aritcle_vote_row = namedtuple_fetchall(cursor)[0]
 
-        assert user_id == UUID(aritcle_vote.user_id)
-        assert article_id == UUID(aritcle_vote.article_id)
-        assert 1 == aritcle_vote.vote
+        assert user_id == UUID(aritcle_vote_row.user_id)
+        assert article_id == UUID(aritcle_vote_row.article_id)
+        assert 1 == aritcle_vote_row.vote
+
+
+@pytest.mark.django_db
+def test_saving_identical_article_votes_raises_integrity_error(
+        user_id: UUID, article_id: UUID
+):
+    article_vote_repository = ArticleVoteRepository()
+
+    article_vote = ArticleVote(
+        user_id=user_id,
+        article_id=article_id,
+        vote=Vote.UP
+    )
+
+    article_vote_repository.save_article_vote(article_vote)
+
+    with pytest.raises(IntegrityError) as exception_info:
+        article_vote_repository.save_article_vote(article_vote)
+    exception_info.match('UNIQUE constraint failed: article_vote.user_id, '
+                         'article_vote.article_id')
