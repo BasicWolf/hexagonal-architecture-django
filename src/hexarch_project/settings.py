800b0b4581d6821011f1e7bc4831d1374ac94932
Zaur Nasibov <zaur@znasibov.info>
2021-01-13 20:10:21 +0200

    Add Logging configuration to settings

--- src/hexarch_project/settings.py
+++ src/hexarch_project/settings.py
@@ -119,3 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
