ff11ec3e2a6a341379a81d0b6bfc6a8ecbf9f1e8
Zaur Nasibov <zaur@znasibov.info>
2021-01-14 21:11:15 +0200

    Completely remove views.py

--- src/myapp/urls.py
+++ src/myapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from django.apps import apps as django_apps
 
-from myapp import views
 from myapp.application.adapter.api.http.article_vote_view import ArticleVoteView
 from myapp.apps import MyAppConfig
 
@@ -10,6 +9,5 @@ app_config: MyAppConfig = django_apps.get_containing_app_config('myapp')
 article_vote_view: ArticleVoteView = app_config.container['article_vote_view']
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('article_vote', article_vote_view)
 ]
