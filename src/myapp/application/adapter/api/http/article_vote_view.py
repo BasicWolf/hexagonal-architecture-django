c7156fd7cd8124bcfc11da2d6046f98e739d2bcc
Zaur Nasibov <zaur@znasibov.info>
2021-01-14 21:12:40 +0200

    Rudimentary working article_vote_view.py

--- src/myapp/application/adapter/api/http/article_vote_view.py
+++ src/myapp/application/adapter/api/http/article_vote_view.py
@@ -5,21 +5,25 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from myapp.application.adapter.api.http.serializer.cast_article_vote_command_serializer import \
-    CastArticleVoteCommandSerializer
-from myapp.application.domain.model.article_vote import ArticleVote
+from myapp.application.adapter.api.http.serializer.cast_article_vote_command_deserializer import \
+    CastArticleVoteCommandDeserializer
+from myapp.application.ports.api.cast_article_vote.cast_article_vote_result import \
+    VoteCastResult, CastArticleVoteResult
 from myapp.application.ports.api.cast_article_vote.cast_aticle_vote_use_case import (
     CastArticleVoteUseCase
 )
 from myapp.application.ports.api.cast_article_vote.vote_already_cast import (
     VoteAlreadyCast
 )
+from myapp.application.util.assert_never import assert_never
 
 
 class ArticleVoteView(APIView):
+    cast_article_vote_use_case: CastArticleVoteUseCase = None
+
     def __init__(self, cast_article_vote_use_case: CastArticleVoteUseCase):
+        self.cast_article_vote_use_case = cast_article_vote_use_case
         super().__init__()
-        self._cast_article_vote_use_case = cast_article_vote_use_case
 
     def post(self, request: Request) -> Response:
         serializer = CastArticleVoteCommandDeserializer(data=request.data)
@@ -28,16 +32,14 @@ class ArticleVoteView(APIView):
 
         cast_article_vote_command = serializer.create()
 
-        cast_article_vote_result = self._cast_article_vote_use_case.cast_article_vote(
+        result = self.cast_article_vote_use_case.cast_article_vote(
             cast_article_vote_command
         )
-        return cast_article_vote_result.handle_by(self)
 
-    def handle_cast_article_vote(self, article_vote: ArticleVote):
-        return Response({}, status=status.HTTP_201_CREATED)
+        response = None
+        if isinstance(result, VoteCastResult):
+            response = Response({}, status=status.HTTP_201_CREATED)
+        else:
+            assert_never(result)
 
-    def handle_vote_already_cast(self, vote_already_cast: VoteAlreadyCast):
-        pass
-
-    def handle_insufficient_karma(self, user_with_insufficient_karma: UUID):
-        pass
+        return response
