e16f8c237def6eb964fb9fbe9d7b76c60dc26e66
Zaur Nasibov <zaur@znasibov.info>
2021-01-14 15:01:29 +0200

    Only view should be exposed in IoC container configuration

--- src/myapp/hexagonal_configuration.py
+++ src/myapp/hexagonal_configuration.py
@@ -1,3 +1,4 @@
+from myapp.application.adapter.api.http.article_vote_view import ArticleVoteView
 from myapp.application.adapter.spi.persistence.repository.article_vote_repository import \
     ArticleVoteRepository
 from myapp.application.adapter.spi.persistence.repository.vote_casting_user_repository import \
@@ -5,20 +6,24 @@ from myapp.application.adapter.spi.persistence.repository.vote_casting_user_repo
 from myapp.application.service.post_rating_service import PostRatingService
 
 
-def build_production_configuration():
+def build_production_ioc_container():
     _article_vote_repository = ArticleVoteRepository()
     article_vote_exists_adapter = _article_vote_repository
     save_article_vote_adapter = _article_vote_repository
 
     get_vote_casting_user_adapter = VoteCastingUserRepository()
 
-    cast_article_vote_use_case = PostRatingService(
+    _cast_article_vote_use_case = PostRatingService(
         article_vote_exists_adapter,
         get_vote_casting_user_adapter,
         save_article_vote_adapter
     )
 
+    article_vote_view = ArticleVoteView.as_view(
+        cast_article_vote_use_case=_cast_article_vote_use_case
+    )
+
     return {
-        'cast_article_vote_use_case': cast_article_vote_use_case
+        'article_vote_view': article_vote_view
     }
 
