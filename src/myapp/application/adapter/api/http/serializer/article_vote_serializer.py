d40ca2bdc4d15b90c88def3229c88b3eeab97270
Zaur Nasibov <zaur@znasibov.info>
2021-01-20 19:13:55 +0200

    Add DRF EnumField
    Need something to serialize enumerations

--- src/myapp/application/adapter/api/http/serializer/article_vote_serializer.py
+++ src/myapp/application/adapter/api/http/serializer/article_vote_serializer.py
@@ -1,10 +1,12 @@
+from rest_enumfield import EnumField
 from rest_framework import serializers
 
 from myapp.application.domain.model.article_vote import ArticleVote
+from myapp.application.domain.model.vote import Vote
 
 
 class ArticleVoteSerializer(serializers.Serializer[ArticleVote]):
     id = serializers.UUIDField()
     user_id = serializers.UUIDField()
     article_id = serializers.UUIDField()
-    vote = serializers.CharField()
+    vote = EnumField(Vote)
