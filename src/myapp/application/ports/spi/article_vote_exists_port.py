5306aa6447f6e061fa00a6c10f6b254196ba99e7
Zaur Nasibov <zaur@znasibov.info>
2021-03-27 18:51:19 +0200

    Remove unused ArticleVoteExistsPort

--- src/myapp/application/ports/spi/article_vote_exists_port.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol
-from uuid import UUID
-
-
-class ArticleVoteExistsPort(Protocol):
-    def article_vote_exists(self, user_id: UUID, article_id: UUID) -> bool:
-        raise NotImplementedError()
