c2a76135b8fc03b3b20c587dd8b4c5755826903c
Zaur Nasibov <zaur@znasibov.info>
2021-04-30 20:46:55 +0300

    Add api/ prefix to API urls

--- src/hexarch_project/urls.py
+++ src/hexarch_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('myapp.urls')),
+    path('api/', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
