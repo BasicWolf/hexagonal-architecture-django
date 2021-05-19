694beb0d773a1ac9eefe45272c0228c245e33796
Zaur Nasibov <zaur@znasibov.info>
2021-05-19 22:29:56 +0300

    GetVotingUserPort is a protocol!

--- src/myapp/application/ports/spi/get_voting_user_port.py
+++ src/myapp/application/ports/spi/get_voting_user_port.py
@@ -1,8 +1,9 @@
+from typing import Protocol
 from uuid import UUID
 
 from myapp.application.domain.model.voting_user import VotingUser
 
 
-class GetVotingUserPort:
+class GetVotingUserPort(Protocol):
     def get_voting_user(self, user_id: UUID, article_id: UUID) -> VotingUser:
         raise NotImplementedError()
