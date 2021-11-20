9b17488068dbefcebf7cd34b21d050d8f3909f79
Zaur Nasibov <zaur@znasibov.info>
2021-11-20 12:02:10 +0200

    Use dataclass for Karma
    Dataclass removes code clutter and provides automatic __str__ and __cmp__ methods.

--- src/myapp/application/domain/model/karma.py
+++ src/myapp/application/domain/model/karma.py
@@ -1,12 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Karma:
-    _value: int
-
-    def __init__(self, value: int):
-        self._value = value
-
-    @property
-    def value(self) -> int:
-        return self._value
+    value: int
 
     def enough_for_voting(self) -> bool:
-        return self._value >= 5
+        return self.value >= 5
+
+    def __str__(self):
+        return f"Karma({self.value})"
