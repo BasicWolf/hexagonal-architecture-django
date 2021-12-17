b6170101ff73e7721b8e19ee9e62b5495fceb0fd
Zaur Nasibov <zaur@znasibov.info>
2021-12-17 21:07:33 +0200

    Introduce interface with article_vote property for all cast results

--- src/myapp/application/domain/model/cast_article_vote_result.py
+++ src/myapp/application/domain/model/cast_article_vote_result.py
@@ -1,27 +1,46 @@
 from dataclasses import dataclass
-from typing import Union
+from typing import Optional, Protocol, Union
 
+from myapp.application.domain.model.article_vote import ArticleVote
 from myapp.application.domain.model.identifier.article_id import ArticleId
 from myapp.application.domain.model.identifier.user_id import UserId
 from myapp.application.domain.model.vote import Vote
 
 
+class CastArticleVoteResultProtocol(Protocol):
+    @property
+    def article_vote(self) -> Optional[ArticleVote]:
+        raise NotImplementedError()
+
+
 @dataclass
-class InsufficientKarma:
+class InsufficientKarma(CastArticleVoteResultProtocol):
     user_id: UserId
 
+    @property
+    def article_vote(self) -> Optional[ArticleVote]:
+        return None
+
 
 @dataclass
-class VoteAlreadyCast:
+class VoteAlreadyCast(CastArticleVoteResultProtocol):
     user_id: UserId
     article_id: ArticleId
 
+    @property
+    def article_vote(self) -> Optional[ArticleVote]:
+        return None
+
 
 @dataclass
-class VoteSuccessfullyCast:
+class VoteSuccessfullyCast(CastArticleVoteResultProtocol):
     user_id: UserId
     article_id: ArticleId
     vote: Vote
+
+    @property
+    def article_vote(self) -> Optional[ArticleVote]:
+        return ArticleVote(self.article_id, self.vote)
 
 
 CastArticleVoteResult = Union[VoteSuccessfullyCast, InsufficientKarma, VoteAlreadyCast]
---
+++ tests/test_myapp/application/domain/model/test_cast_article_vote_result.py
@@ -0,0 +1,33 @@
+from uuid import uuid4
+
+from myapp.application.domain.model.article_vote import ArticleVote
+from myapp.application.domain.model.cast_article_vote_result import (
+    InsufficientKarma, VoteAlreadyCast,
+    VoteSuccessfullyCast
+)
+from myapp.application.domain.model.identifier.article_id import ArticleId
+from myapp.application.domain.model.identifier.user_id import UserId
+from myapp.application.domain.model.vote import Vote
+
+
+def test_article_vote_property_of_successfully_cast_vote():
+    vote_cast_result = VoteSuccessfullyCast(
+        UserId(uuid4()),
+        ArticleId('1aaaaaaa-0000-0000-0000-000000000000'),
+        Vote.UP
+    )
+
+    assert vote_cast_result.article_vote == ArticleVote(
+        ArticleId('1aaaaaaa-0000-0000-0000-000000000000'),
+        Vote.UP
+    )
+
+
+def test_vote_already_cast_result_has_no_article_vote():
+    vote_already_cast = VoteAlreadyCast(UserId(uuid4()), ArticleId(uuid4()))
+    assert vote_already_cast.article_vote is None
+
+
+def test_insufficient_karma_result_has_no_article_vote():
+    insufficient_karma = InsufficientKarma(UserId(uuid4()))
+    assert insufficient_karma.article_vote is None
