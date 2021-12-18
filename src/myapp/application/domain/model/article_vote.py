da61d015d63ff9dabfc935879aa4011cb0864905
Zaur Nasibov <zaur@znasibov.info>
2021-12-18 18:21:45 +0200

    Add Union type wrapping "Cast" and "Uncast" article votes

--- src/myapp/application/domain/model/article_vote.py
+++ src/myapp/application/domain/model/article_vote.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Union
 
 from myapp.application.domain.model.identifier.article_id import ArticleId
 from myapp.application.domain.model.identifier.user_id import UserId
@@ -10,3 +11,11 @@ class ArticleVote:
     article_id: ArticleId
     user_id: UserId
     vote: Vote
+
+
+@dataclass
+class UncastArticleVote:
+    article_id: ArticleId
+
+
+CastOrUncastArticleVote = Union[ArticleVote, UncastArticleVote]
--- src/myapp/application/ports/spi/find_article_vote_port.py
+++ src/myapp/application/ports/spi/find_article_vote_port.py
@@ -1,10 +1,12 @@
 from typing import Protocol
 
-from myapp.application.domain.model.article_vote import ArticleVote
+from myapp.application.domain.model.article_vote import (
+    CastOrUncastArticleVote
+)
 from myapp.application.domain.model.identifier.article_id import ArticleId
 from myapp.application.domain.model.identifier.user_id import UserId
 
 
 class FindArticleVotePort(Protocol):
-    def find_article_vote(self, article_id: ArticleId, user_id: UserId) -> ArticleVote:
+    def find_article_vote(self, article_id: ArticleId, user_id: UserId) -> CastOrUncastArticleVote:
         raise NotImplementedError()
