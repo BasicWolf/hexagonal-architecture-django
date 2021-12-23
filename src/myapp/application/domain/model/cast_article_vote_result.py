a141a5a8244cd92aa15dfbad153dbc7c74c49c50
Zaur Nasibov <zaur@znasibov.info>
2021-12-23 21:58:29 +0200

    Remove unnecessary CastArticleVoteResultProtocol
    There is no need for a CastArticleVoteResult to carry ArticleVote object

--- src/myapp/application/domain/model/cast_article_vote_result.py
+++ src/myapp/application/domain/model/cast_article_vote_result.py
@@ -1,51 +1,29 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Optional, Protocol, TYPE_CHECKING, Union
+from typing import Union
 
 from myapp.application.domain.model.identifier.article_id import ArticleId
 from myapp.application.domain.model.identifier.user_id import UserId
 from myapp.application.domain.model.vote import Vote
 
-if TYPE_CHECKING:
-    from myapp.application.domain.model.article_vote import ArticleVote
-
-
-class CastArticleVoteResultProtocol(Protocol):
-    @property
-    def article_vote(self) -> Optional[ArticleVote]:
-        raise NotImplementedError()
-
 
 @dataclass
-class InsufficientKarma(CastArticleVoteResultProtocol):
+class InsufficientKarma:
     user_id: UserId
 
-    @property
-    def article_vote(self) -> Optional[ArticleVote]:
-        return None
-
 
 @dataclass
-class VoteAlreadyCast(CastArticleVoteResultProtocol):
+class VoteAlreadyCast:
     user_id: UserId
     article_id: ArticleId
 
-    @property
-    def article_vote(self) -> Optional[ArticleVote]:
-        return None
-
 
 @dataclass
-class VoteSuccessfullyCast(CastArticleVoteResultProtocol):
+class VoteSuccessfullyCast:
     user_id: UserId
     article_id: ArticleId
     vote: Vote
-
-    @property
-    def article_vote(self) -> Optional[ArticleVote]:
-        from myapp.application.domain.model.article_vote import ArticleVote
-        return ArticleVote(self.article_id, self.user_id, self.vote)
 
 
 CastArticleVoteResult = Union[VoteSuccessfullyCast, InsufficientKarma, VoteAlreadyCast]
