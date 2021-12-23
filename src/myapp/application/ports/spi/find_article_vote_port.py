5c7d498d39beaeac2e552f5f791e9faeb2e8839d
Zaur Nasibov <zaur@znasibov.info>
2021-12-23 19:23:07 +0200

    Fix formatting

--- src/myapp/application/ports/spi/find_article_vote_port.py
+++ src/myapp/application/ports/spi/find_article_vote_port.py
@@ -9,8 +9,8 @@ from myapp.application.domain.model.identifier.user_id import UserId
 
 class FindArticleVotePort(Protocol):
     def find_article_vote(
-            self,
-            article_id: ArticleId,
-            user_id: UserId
+        self,
+        article_id: ArticleId,
+        user_id: UserId
     ) -> CastOrUncastArticleVote:
         raise NotImplementedError()
