c2ebfafbdce906c303c9ac2056a062990854e423
Zaur Nasibov <zaur@znasibov.info>
2021-12-28 21:41:23 +0200

    Test dispatching events

--- tests/test_myapp/eventlib/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from myapp.eventlib.event_dispatcher import EventDispatcher
-
-
-@pytest.fixture
-def event_dispatcher() -> EventDispatcher:
-    return EventDispatcher()
--- tests/test_myapp/eventlib/test_event_dispatcher.py
+++ tests/test_myapp/eventlib/test_event_dispatcher.py
@@ -1,14 +1,22 @@
 from dataclasses import dataclass
 
+import pytest
+
 from myapp.eventlib.event import Event
 from myapp.eventlib.event_dispatcher import EventDispatcher
 
 
-def test_register_event_handler(event_dispatcher: EventDispatcher):
-    @dataclass
-    class SampleEvent(Event):
-        samples: int = 0
+@pytest.fixture
+def event_dispatcher() -> EventDispatcher:
+    return EventDispatcher()
 
+
+@dataclass
+class SampleEvent(Event):
+    samples: int = 0
+
+
+def test_register_event_handler(event_dispatcher: EventDispatcher):
     def sample_event_handler(sample_event: SampleEvent):
         pass
 
@@ -17,13 +25,9 @@ def test_register_event_handler(event_dispatcher: EventDispatcher):
     assert event_dispatcher.handlers[SampleEvent] == [sample_event_handler]
 
 
-def test_dispatched_event_gets_handled(
+def test_handle_dispatched_event(
     event_dispatcher: EventDispatcher
 ):
-    @dataclass
-    class SampleEvent(Event):
-        samples: int = 0
-
     handled_samples = 0
 
     def sample_event_handler(sample_event: SampleEvent):
@@ -34,3 +38,22 @@ def test_dispatched_event_gets_handled(
     event_dispatcher.dispatch(SampleEvent(samples=100))
 
     assert handled_samples == 100
+
+
+def test_handle_dispatched_event_by_two_handlers(
+    event_dispatcher: EventDispatcher
+):
+    class SampleEventHandler:
+        handled_samples = 0
+
+        def __call__(self, sample_event: SampleEvent):
+            self.handled_samples = sample_event.samples
+
+    first_handler = SampleEventHandler()
+    second_handler = SampleEventHandler()
+    event_dispatcher.register_handler(SampleEvent, first_handler)
+    event_dispatcher.register_handler(SampleEvent, second_handler)
+    event_dispatcher.dispatch(SampleEvent(samples=37))
+
+    assert first_handler.handled_samples == 37
+    assert second_handler.handled_samples == 37
