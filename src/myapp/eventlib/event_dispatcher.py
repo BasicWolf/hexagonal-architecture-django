a711592d48912d49460b4239fdab211248c9e00e
Zaur Nasibov <zaur@znasibov.info>
2021-12-31 22:32:45 +0200

    Make EventDispatcher.handlers a read-only property

--- src/myapp/eventlib/event_dispatcher.py
+++ src/myapp/eventlib/event_dispatcher.py
@@ -4,20 +4,25 @@ from myapp.eventlib.event import Event
 
 T_contra = TypeVar('T_contra', bound=Event, covariant=True)
 EventHandlerBaseType = Callable[[T_contra], None]
+EventHandlersCollectionType = Dict[Type[Event], List[EventHandlerBaseType]]
 
 
 class EventDispatcher:
-    handlers: Dict[Type[Event], List[EventHandlerBaseType]] = {}
+    _handlers: EventHandlersCollectionType = {}
 
     def register_handler(
         self,
         event_type: Type[T_contra],
         event_handler: Callable[[T_contra], None]
     ):
-        self.handlers.setdefault(event_type, list())
-        self.handlers[event_type].append(event_handler)
+        self._handlers.setdefault(event_type, list())
+        self._handlers[event_type].append(event_handler)
 
     def dispatch(self, event: Event):
-        event_handlers = self.handlers.get(type(event), list())
+        event_handlers = self._handlers.get(type(event), list())
         for event_handler in event_handlers:
             event_handler(event)
+
+    @property
+    def handlers(self) -> EventHandlersCollectionType:
+        return self._handlers
