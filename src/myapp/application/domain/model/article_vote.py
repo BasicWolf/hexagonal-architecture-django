8856ec8ad30d02ded76069c385d0ab5ac5dc0bef
Zaur Nasibov <zaur@znasibov.info>
2022-01-10 22:33:55 +0200

    Remove unused method ArticleVote.to_already_voted_result()

--- src/myapp/application/domain/model/article_vote.py
+++ src/myapp/application/domain/model/article_vote.py
@@ -5,9 +5,6 @@ from dataclasses import dataclass
 from myapp.application.domain.model.identifier.article_id import ArticleId
 from myapp.application.domain.model.identifier.user_id import UserId
 from myapp.application.domain.model.vote import Vote
-from myapp.application.domain.model.vote_for_article_result import (
-    AlreadyVotedResult
-)
 
 
 @dataclass
@@ -15,6 +12,3 @@ class ArticleVote:
     article_id: ArticleId
     user_id: UserId
     vote: Vote
-
-    def to_already_voted_result(self) -> AlreadyVotedResult:
-        return AlreadyVotedResult(self.article_id, self.user_id)
