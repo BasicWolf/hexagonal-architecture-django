7b5737d0883c8c7a8db76fd606bc796e4f6774fe
Zaur Nasibov <zaur@znasibov.info>
2022-12-05 13:57:34 +0200

    Explain importing/exporting code in models.py

--- src/myapp/models.py
+++ src/myapp/models.py
@@ -5,6 +5,7 @@ from myapp.application.adapter.spi.persistence.entity.voting_user_entity import 
     VotingUserEntity
 )
 
+# A way to explicitly tell linters that the imported classes are used
 __all__ = [
     'ArticleVoteEntity',
     'VotingUserEntity'
